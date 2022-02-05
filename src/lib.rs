50d89ddc9d1340c6f3ce50ee96874993ded4ae72
yuma140902 <yuma140902@gmail.com>
2022-02-06 01:14:39 +0900

    Feat: listサブコマンドを実装

--- src/lib.rs
+++ src/lib.rs
@@ -225,6 +225,115 @@ pub fn uninstall(_install_registry: InstallRegistry, _name: String) {
     todo!()
 }
 
-pub fn list(_install_registry: Option<InstallRegistry>) {
-    todo!()
+pub fn list(install_registry: Option<InstallRegistry>) {
+    if install_registry.is_none() || install_registry == Some(InstallRegistry::Cli) {
+        list_cli();
+    }
+    if install_registry.is_none() || install_registry == Some(InstallRegistry::SendTo) {
+        list_sendto();
+    }
+}
+
+fn list_cli() {
+    let s4dir = get_s4dir();
+    let programs = get_installed_programs(&s4dir);
+
+    match programs {
+        Ok(programs) => {
+            eprintln!(
+                "{} programs installed in {}",
+                programs.len(),
+                s4dir
+                    .map(|dir| dir.to_string_lossy().to_string())
+                    .unwrap_or_default()
+            );
+            for program in programs {
+                eprintln!(
+                    "    {}",
+                    program
+                        .file_name()
+                        .map(|s| s.to_string_lossy())
+                        .unwrap_or_default()
+                );
+            }
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err)
+        }
+    }
+}
+
+fn list_sendto() {
+    let sendto_dir = get_sendto_dir();
+    let programs = get_installed_programs(&sendto_dir);
+
+    match programs {
+        Ok(programs) => {
+            eprintln!(
+                "{} programs installed in {}",
+                programs.len(),
+                sendto_dir
+                    .map(|dir| dir.to_string_lossy().to_string())
+                    .unwrap_or_default()
+            );
+            for program in programs {
+                eprintln!(
+                    "    {}",
+                    program
+                        .file_name()
+                        .map(|s| s.to_string_lossy())
+                        .unwrap_or_default()
+                );
+            }
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err)
+        }
+    }
+}
+
+fn get_installed_programs(dir: &Option<impl AsRef<Path>>) -> Result<Vec<PathBuf>, std::io::Error> {
+    if let Some(dir) = dir {
+        let program_exts = {
+            let mut p = get_program_exts();
+            p.push("ps1".to_string());
+            p.push("lnk".to_string());
+            p
+        };
+
+        let mut programs = Vec::new();
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if !path.is_file() {
+                continue;
+            }
+            if is_program_file(&path, &program_exts) {
+                programs.push(path);
+            }
+        }
+        Ok(programs)
+    } else {
+        Ok(Vec::with_capacity(0))
+    }
+}
+
+fn get_program_exts() -> Vec<String> {
+    let pathext = std::env::var("PATHEXT");
+    if let Ok(pathext) = pathext {
+        pathext
+            .split(';')
+            .map(|s| s.trim_start_matches('.').to_ascii_lowercase())
+            .collect()
+    } else {
+        Vec::with_capacity(0)
+    }
+}
+
+fn is_program_file(file: impl AsRef<Path>, program_exts: &Vec<String>) -> bool {
+    if let Some(ext) = file.as_ref().extension() {
+        program_exts.contains(&ext.to_string_lossy().to_ascii_lowercase())
+    } else {
+        false
+    }
 }
